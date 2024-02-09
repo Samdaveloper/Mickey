5e87fe9a423acb238fbfcaf100764a594ffc9cb8
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2024-02-09 21:26:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,36 +4,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+API_ID = int(getenv("API_ID", 23761673))
+API_HASH = getenv("API_HASH", "4eacc40d2ce3ecf3ea356ff1674136e7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 5350929381))
+OWNER_ID = int(getenv("OWNER_ID", 5333769332))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Nexa_Verse")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "DadEyeBotz")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Dev_Arora_0981")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "Marathi_Chatting_143")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "caption_marathi_status")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "its_me_max")
 
 # Random Start Images
 IMG = [
-    "https://te.legra.ph/file/5bf629d10afd4af953585.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://te.legra.ph/file/7a321b99fe99d9d8b5117.jpg",
-    "https://te.legra.ph/file/c482a7e55b459ffe07502.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/0879fbdb307005c1fa8ab.jpg",
-    "https://telegra.ph//file/19e3a9d5c0985702497fb.jpg",
-    "https://telegra.ph//file/b5fa277081dddbddd0b12.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/96e96245fe1afb82d0398.jpg",
-    "https://telegra.ph//file/fb140807129a3ccb60164.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/09c9ea0e2660efae6f62a.jpg",
-    "https://telegra.ph//file/3b59b15e1914b4fa18b71.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/efb26cc17eef6fe82d910.jpg",
-    "https://telegra.ph//file/ab4925a050e07b00f63c5.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/d169a77fd52b46e421414.jpg",
-    "https://telegra.ph//file/dab9fc41f214f9cded1bb.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/e05d6e4faff7497c5ae56.jpg",
-    "https://telegra.ph//file/1e54f0fff666dd53da66f.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/18e98c60b253d4d926f5f.jpg",
     "https://telegra.ph//file/b1f7d9702f8ea590b2e0c.jpg",
-    "https://telegra.ph//file/7bb62c8a0f399f6ee1f33.jpg",
+    "https://telegra.ph/file/d8869bcc0797248090105.jpg",
     "https://telegra.ph//file/dd00c759805082830b6b6.jpg",
     "https://telegra.ph//file/3b996e3241cf93d102adc.jpg",
     "https://telegra.ph//file/610cc4522c7d0f69e1eb8.jpg",
